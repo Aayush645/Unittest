c2b11d18c04ae19d0931a1447fb065963401ba45
Aayush Kamboj <aayushkamboj07@gmail.com>
2021-10-26 17:44:07 +0530

    Changes made

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import unittest
 class testCheck(unittest.TestCase):  
            
       def test1(self):
-          result=1+2+6
-          self.assertEqual(result,6)
+          result=1+2+7
+          self.assertEqual(result,10)
 
 
 if __name__=='__main__':
