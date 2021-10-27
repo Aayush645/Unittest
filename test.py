7d1f7f6706635ec2e7fd5d0f47cd69668cacfd33
Aayush Kamboj <aayushkamboj07@gmail.com>
2021-10-27 11:16:16 +0530

    third commit

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import unittest
 class testCheck(unittest.TestCase):  
            
       def test1(self):
-          result=1+2+7
-          self.assertEqual(result,10)
+          result=1+2+8
+          self.assertEqual(result,11)
 
 
 if __name__=='__main__':
